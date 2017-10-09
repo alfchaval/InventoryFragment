e034c6a6d6cbf623fa911030be170232d79caa79
alfchaval <alfchaval@hotmail.com>
2017-10-09 12:52:14 +0200

    1

---
+++ app/src/main/java/com/example/usuario/inventory/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.usuario.inventory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUser, edtPassword;
+    CheckBox chkRemember;
+    Button btnSignUp, btnFacebookSignU, btnGoogleSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtUser = (EditText)findViewById(R.id.edtUser);
+        edtPassword = (EditText)findViewById(R.id.edtPassword);
+        chkRemember = (CheckBox)findViewById(R.id.chkRemember);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, InventoryActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnFacebookSignU = (Button) findViewById(R.id.btnFacebookSignUp);
+        btnGoogleSignUp = (Button) findViewById(R.id.btnGoogleSignUp);
+    }
+}
---
+++ app/src/main/java/com/example/usuario/inventory/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.usuario.inventory;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        },2000);
+
+
+    }
+}
